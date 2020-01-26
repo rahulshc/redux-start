70369f01236aef2f140609b5c7462ccdd3ce327e
Swastika Library <rahul1.shc@gmail.com>
2020-01-26 16:16:32 +0530

    updating states immutably and outsourced action types

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,3 +1,4 @@
+import * as actionTypes from '../../store/actions';
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import CounterControl from '../../components/CounterControl/CounterControl';
@@ -13,6 +14,15 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
                 <CounterControl label="Add 5" clicked={this.props.onADDCounter}  />
                 <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter}  />
+                <hr/>
+                {/*Clicking on store result returns the current state only hence the output of current is always same */}
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(strResult => (
+                        <li key={strResult.id} onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>
+                    ))}
+                    
+                </ul>
             </div>
         );
     }
@@ -20,16 +30,19 @@ class Counter extends Component {
 
 const mapStateToProps= state => {
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch ({type: 'INCREMENT'}),
-        onADDCounter: () => dispatch ({type: 'ADD', value: 5}),
-        onDecrementCounter: () => dispatch ({type: 'DECREMENT'}),
-        onSubtractCounter: () => dispatch ({type: 'SUBTRACT', value: 5})
+        onIncrementCounter: () => dispatch ({type: actionTypes.INCREMENT}),
+        onADDCounter: () => dispatch ({type: actionTypes.ADD, value: 5}),
+        onDecrementCounter: () => dispatch ({type: actionTypes.DECREMENT}),
+        onSubtractCounter: () => dispatch ({type: actionTypes.SUBTRACT, value: 5}),
+        onStoreResult: () => dispatch({type: actionTypes.STORE_RESULT}),
+        onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, resultElId: id})
     };
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,28 +1,53 @@
+import * as actionTypes from './actions';
+
 const initialCounter = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 const reducer = (state=initialCounter, action) => {
-    if(action.type === 'INCREMENT'){
-        return {
-            counter: state.counter+1
-        }
+    switch(action.type){
+        case actionTypes.INCREMENT:
+            /*returning state like this will overwrite matching property 
+        name (counter here) and replace it means results will be lost 
+            return {
+                counter: state.counter +1
+            }*/
+            const newState = Object.assign({}, state);
+            newState.counter=state.counter+1;
+            return newState;
+        //no break needed as return already breaks it
+        case actionTypes.DECREMENT:
+            return {
+                ...state,
+                counter: state.counter-1
+            }
+        case actionTypes.ADD:
+            return {
+                ...state,
+                counter: state.counter + action.value
+            }
+        case actionTypes.SUBTRACT:
+            return {
+                ...state,
+                counter: state.counter - action.value
+            }
+        case actionTypes.STORE_RESULT:
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: state.counter})//state.results.push will push to the original state results array
+            }
+        case actionTypes.DELETE_RESULT:
+           /* const id=2;
+            const newArray = [...state.results];
+            newArray.splice(id,1);*/
+            const updatedArray = state.results.filter(result => result.id !== action.resultElId)
+            return {
+                ...state,
+                results: updatedArray
+            }
+        default:
+            return state
     }
-    if(action.type === 'DECREMENT'){
-        return {
-            counter: state.counter-1
-        }
-    }
-    if(action.type === 'ADD'){
-        return {
-            counter: state.counter+action.value
-        }
-    }
-    if(action.type === 'SUBTRACT'){
-        return {
-            counter: state.counter-action.value
-        }
-    }
-    return state;
 }
 
 export default reducer;
